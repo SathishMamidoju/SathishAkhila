a06d1be9b5107f901f725ea2aaa4ddbebeeb6ad6
SathishMamidoju <sathishmamidoju@gmail.com>
2022-11-02 12:38:16 +0530

    Recorrect the code

---
+++ CheckPrime.js
@@ -0,0 +1,25 @@
+let x=25;
+
+let count=0;
+
+for(let i=1 ; i<=x ; i++){
+
+if(i%x==0){
+
+count++;
+
+}
+
+}
+
+if(count==2){
+
+console.log("Prime");
+
+}else{
+
+console.log("NotPrime");
+
+}
+
+
